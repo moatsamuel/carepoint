a71bef1e2c5c52cf2d8655d46a62bf567349a62b
Mofe <greatmof@gmail.com>
2026-09-24 03:30:12 -0500

    Add python-dotenv and configure SECRET_KEY

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -1,5 +1,10 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 
+load_dotenv()
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 from pkg import routes
